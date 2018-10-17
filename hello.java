e441bf6154dcb493a43414508df3732751799df3
parkdoje <44152370+parkdoje@users.noreply.github.com>
2018-10-17 14:31:34 +0900

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ class hello
     {
         public static void main (String argv[]){
             System.out.println("hello!");
-	System.out.println("¹ÚµµÁ¦");
+	System.out.println("pardoje");
         };
     };
