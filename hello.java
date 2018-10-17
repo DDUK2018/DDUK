c8d2bd39f69671599804dba5d1cc0d522e848f26
GooodGu <pig5973@naver.com>
2018-10-17 14:37:50 +0900

    x
    x

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 class hello
     {
         public static void main (String argv[]){
-            System.out.println("hello!");
+            System.out.println("hello! GoodGu");
 	System.out.println("My username is  Redping");
-        };
-    };
+        }
+    }
