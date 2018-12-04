38807726105b58e4f663fd59ad27dfb26f48ced6
parkdoje <44152370+parkdoje@users.noreply.github.com>
2018-12-05 05:32:55 +0900

    add

--- hello.java
+++ hello.java
@@ -5,7 +5,6 @@ class hello
             System.out.println("My username is  Redping");
             System.out.println("My name is park do je");
             System.out.println("My name is Minsu");
-            
-            
+            System.out.println("My name is chan");           
         }
     }
