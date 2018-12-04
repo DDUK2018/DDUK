e08c6c60b3cb9872291f02a91a2e66c8c125cc05
parkdoje <44152370+parkdoje@users.noreply.github.com>
2018-12-05 05:04:44 +0900

    add

--- hello.java
+++ hello.java
@@ -2,7 +2,8 @@ class hello
     {
         public static void main (String argv[]){
             System.out.println("hello! PARKDOJEsasa");
-	System.out.println("My username is  Redping");
-	System.out.println("My name is park do je");
+            System.out.println("My username is  Redping");
+            System.out.println("My name is park do je");
+            System.out.println("My name is Minsu");
         }
     }
