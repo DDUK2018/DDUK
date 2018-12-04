f2c8f5ac3ef4b76b91709b9f89e718a2cebc2acd
changhwan <walkj98@naver.com>
2018-12-05 04:41:54 +0900

    add println("My name is park doje")

--- hello.java
+++ hello.java
@@ -1,7 +1,8 @@
 class hello
     {
         public static void main (String argv[]){
-            System.out.println("hello! GoodGu");
+            System.out.println("hello! PARKDOJEsasa");
 	System.out.println("My username is  Redping");
+	System.out.println("My name is park do je");
         }
     }
